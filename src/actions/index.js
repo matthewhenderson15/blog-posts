1b59ae77f2798483ddd669662c3b7a6d63b749c8
Matthew Henderson <info@matthewahenderson.com>
2022-09-17 13:24:48 -0400

    Added lodash for map and unique functions to be used in fetchPostsAndUsers function; wired new function up to PostList component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,22 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+// Gets the posts, then gets unique user ids of the posts using lodash; unique ids then used in fetchUser action creator
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
+// Individual action creator for fetching posts
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
+// Individual action creator for fetching users by user id
 export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
